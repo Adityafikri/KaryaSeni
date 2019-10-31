f3d41dfe8e333fdfa3d0d4b7ea92430fabd1a8af
Aditya880 <adityafikriansyah8879@gmail.com>
2019-10-31 17:07:24 +0700

    Karya Seni Aditya

---
+++ app/src/main/java/com/example/karyaseni/Activity_Login.java
@@ -0,0 +1,15 @@
+package com.example.karyaseni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity_Login extends AppCompatActivity {
+
+
+    @Override
+
+
+}
+
